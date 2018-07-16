56b1731194b9a5f8ca4021380ad8616dfa35624e
chandrahassan <chan.hassan@gmail.com>
2018-07-15 23:23:41 -0500

    inital

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Form from './component/Form'
+
+class App extends React.Component{
+
+
+
+    render(){
+        return(
+           <div>
+               <Form/>
+           </div>
+        )
+    }
+}
+
+export default App;
